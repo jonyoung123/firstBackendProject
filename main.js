24ae60d51f749c6ec38d39a31d7ff88af59d8288
John Eke <stephenjohn5646@gmail.com>
2022-09-20 16:42:10 +0100

    reverse a number

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log('Hello World!!');
+// Jvascript code to reverse a number
+console.time('timer');
+function reverseNum(num){
+    newStr = '';
+    numStr = num.toString();
+    for (let i = numStr.length-1; i >= 0; i--){
+        newStr = newStr + numStr[i];
+    }
+    console.log(parseInt(newStr));
+}
+reverseNum(3245);
+console.timeEnd('timer');
+
+console.time('time');
+function reverseNumber(n){
+    n = n.toString();
+    n= n.split("").reverse().join("");
+    console.log(parseInt(n));
+}
+reverseNumber(3245);
+console.timeEnd('time');
